b7a843d7591985b110e5217af6e75c09af4c7b4d
Golam Rabbani <golamrabbani9373@gmail.com>
2024-05-21 00:37:11 +0600

    add product route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import { productRoute } from './app/modules/product/product.route'
 const app: Application = express()
 
 // ! Parser
 app.use(express.json())
 app.use(cors())
+
+// !Product Route
+
+app.use('/api/products', productRoute)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Mongoose Master Server is Running')
