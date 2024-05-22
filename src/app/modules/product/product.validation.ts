66267a2660e8ee8dafa2bd47bbfd06c7ceac039c
Golam Rabbani <golamrabbani9373@gmail.com>
2024-05-22 15:17:13 +0600

    product validation Using Zod complete

--- src/app/modules/product/product.validation.ts
+++ src/app/modules/product/product.validation.ts
@@ -4,13 +4,15 @@ export const productValidationSchema = z.object({
   name: z
     .string({
       invalid_type_error: 'Name must be string',
+      required_error: 'Name is required',
     })
-    .min(0, 'Name is required'),
+    .min(1, { message: 'Name is required And Name must be string' }),
   description: z
     .string({
       invalid_type_error: 'Description must be string',
+      required_error: 'Description is required',
     })
-    .min(1, 'Description is required'),
+    .min(1, { message: 'Description is required' }),
   price: z
     .number({
       invalid_type_error: 'Price must be Number',
@@ -20,41 +22,59 @@ export const productValidationSchema = z.object({
   category: z
     .string({
       invalid_type_error: 'Category must be String',
+      required_error: 'Category is required',
     })
-    .min(1, 'Category is required'),
-  tags: z
-    .array(
-      z.string({
+    .min(1, { message: 'Category is required' }),
+  tags: z.array(
+    z
+      .string({
         invalid_type_error: 'Tags must be String',
-      }),
-    )
-    .nonempty('Tags is required'),
-  variants: z
-    .array(
-      z.object({
-        type: z
-          .string({
-            invalid_type_error: 'Product variants Type must be String',
-            required_error: 'Product variants Type is required',
-          })
-          .min(1, 'Product variants Type is required'),
-
-        value: z
-          .string({
-            invalid_type_error: 'Product variants value must be String',
-            required_error: 'Product variants value is required',
-          })
-          .min(1, 'Product variants value is required'),
-      }),
-    )
-    .nonempty('Variants is required'),
-  inventory: z.object({
-    quantity: z
-      .number({
-        invalid_type_error: 'Product Quantity must be Number',
-        required_error: 'Product Quantity  is required',
+        required_error: 'Tags is required',
       })
-      .min(1, 'Quantity must be a non-negative number and minimum 1'),
-    inStock: z.boolean(),
-  }),
+      .min(1, { message: 'At least one tag is required' }),
+    {
+      invalid_type_error: 'Tags must be String',
+      required_error: 'Tags is required',
+    },
+  ),
+  variants: z.array(
+    z.object({
+      type: z
+        .string({
+          invalid_type_error: 'Product variants Type must be String',
+          required_error: 'Product variants Type is required',
+        })
+        .min(1, {
+          message: 'At least one Product variants Type is required',
+        }),
+
+      value: z
+        .string({
+          invalid_type_error: 'Product variants value must be String',
+          required_error: 'Product variants value is required',
+        })
+        .min(1, {
+          message: 'At least one Product variants value is required',
+        }),
+    }),
+    {
+      required_error: 'variants is required',
+    },
+  ),
+  inventory: z.object(
+    {
+      quantity: z
+        .number({
+          invalid_type_error: 'Product Quantity must be Number',
+          required_error: 'Product Quantity  is required',
+        })
+        .min(1, {
+          message: 'Quantity must be a non-negative number and minimum 1',
+        }),
+      inStock: z.boolean(),
+    },
+    {
+      required_error: 'inventory is required',
+    },
+  ),
 })
